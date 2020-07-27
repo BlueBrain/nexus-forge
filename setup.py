565bf7f64dc9af839626420e6a6a164905f1a083
Pierre-Alexandre Fonta <pierre-alexandre.fonta@epfl.ch>
2020-07-27 12:01:31 +0200

    Use codecov.io for code coverage reports (#48)

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
         "rdflib-jsonld"
     ],
     extras_require={
-        "dev": ["tox", "pytest", "pytest-bdd", "pytest-cov", "pytest-mock"],
+        "dev": ["tox", "pytest", "pytest-bdd", "pytest-cov", "pytest-mock", "codecov"],
         "docs": ["sphinx", "sphinx-bluebrain-theme"],
     },
     classifiers=[
