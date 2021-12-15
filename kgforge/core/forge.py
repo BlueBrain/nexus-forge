188fe36c04422094b3d66735509ae1912dfb0500
Konstantinos Platis <konstantinos.platis@epfl.ch>
2021-12-15 16:50:33 +0100

    Add unit tests for resolver

--- kgforge/core/forge.py
+++ kgforge/core/forge.py
@@ -235,7 +235,7 @@ class KnowledgeGraphForge:
         {
             "firstResolver": {
                 "firstTarget": {
-                    "source": "bbp/agents",
+                    "source": "firstSource",
                 },
                 "secondTarget": {
                     "source": "secondSource",
