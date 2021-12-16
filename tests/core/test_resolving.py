07047130a01735edf9f950fe1b85df37abe7be9a
Konstantinos Platis <konstantinos.platis@epfl.ch>
2021-12-16 13:04:49 +0100

    Move unit test from test_resolving to test_forge

--- tests/core/test_resolving.py
+++ tests/core/test_resolving.py
@@ -13,22 +13,3 @@
 # along with Blue Brain Nexus Forge. If not, see <https://choosealicense.com/licenses/lgpl-3.0/>.
 
 # Placeholder for the test suite for selection of target or resolver from input and configuration.
-
-from kgforge.core import KnowledgeGraphForge
-
-
-class TestResolverAsDict:
-    """
-    Tests the .resolver(as_dict=True) function that returns a dictionary
-    """
-
-    def test_resolver_returns_dict(self, config):
-        forge = KnowledgeGraphForge(config)
-        resolvers_dict = forge.resolvers(as_dict=True)
-        assert type(resolvers_dict) is dict
-
-    def test_resolver_returns_correct_object(self, config):
-        dict_result = {'terms': {'sex': {'source': 'sex.json'}}}
-        forge = KnowledgeGraphForge(config)
-        resolvers_dict = forge.resolvers(as_dict=True)
-        assert resolvers_dict == dict_result
