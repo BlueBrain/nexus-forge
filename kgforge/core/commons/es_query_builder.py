ffd6f3169585f220a1b3e1619f3c23035a4192e6
Mohameth François SY <mohameth.sy@epfl.ch>
2023-05-04 11:54:32 +0200

    Update es_query_builder.py

--- kgforge/core/commons/es_query_builder.py
+++ kgforge/core/commons/es_query_builder.py
@@ -136,7 +136,7 @@ class ESQueryBuilder(QueryBuilder):
                         else "must_not"
                     )
                     k_path = keyword_path
-                    term_or_match = term_or_match = "terms" if isinstance(f.value, list) else "term"
+                    term_or_match = "terms" if isinstance(f.value, list) else "term"
                 else:
                     filter_or_must_or_must_not = (
                         "must"
