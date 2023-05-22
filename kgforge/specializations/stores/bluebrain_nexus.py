e27c25ed4e82263501785007cc1c16d617dd690c
Cristina E. González-Espinoza <crisbeth46@gmail.com>
2023-05-22 11:03:19 +0200

    Removed query building functions.

--- kgforge/specializations/stores/bluebrain_nexus.py
+++ kgforge/specializations/stores/bluebrain_nexus.py
@@ -1040,63 +1040,6 @@ def _error_message(error: HTTPError) -> str:
         return format_message(str(error))
 
 
-def build_sparql_query_statements(context: Context, *conditions) -> Tuple[List, List]:
-    statements = list()
-    filters = list()
-    for index, f in enumerate(*conditions):
-        last_path = f.path[-1]
-        try:
-            last_term = context.terms[last_path]
-        except KeyError:
-            last_term = None
-        if last_path in ["id", "@id"]:
-            property_path = "/".join(f.path[:-1])
-        elif last_path == "@type":
-            minus_last_path = f.path[:-1]
-            minus_last_path.append("type")
-            property_path = "/".join(minus_last_path)
-        else:
-            property_path = "/".join(f.path)
-        try:
-            if (
-                last_path in ["type", "@type"]
-                or last_path in ["id", "@id"]
-                or (last_term is not None and last_term.type == "@id")
-            ):
-                if f.operator == "__eq__":
-                    statements.append(f"{property_path} {_box_value_as_full_iri(f.value)}")
-                elif f.operator == "__ne__":
-                    statements.append(f"{property_path} ?v{index}")
-                    filters.append(f"FILTER(?v{index} != {f.value})")
-                else:
-                    raise NotImplementedError(
-                        f"supported operators are '==' and '!=' when filtering by type or id."
-                    )
-            else:
-                parsed_type, parsed_value = _parse_type(f.value, parse_str=False)
-                value_type = type_map[parsed_type]
-                value = format_type[value_type](parsed_value if parsed_value else f.value)
-                if value_type is CategoryDataType.LITERAL:
-                    if f.operator not in ["__eq__", "__ne__"]:
-                        raise NotImplementedError(
-                            f"supported operators are '==' and '!=' when filtering with a str.")
-                    statements.append(f"{property_path} ?v{index}")
-                    filters.append(f"FILTER(?v{index} = {_box_value_as_full_iri(value)})")
-                else:
-                    statements.append(f"{property_path} ?v{index}")
-                    filters.append(
-                        f"FILTER(?v{index} {sparql_operator_map[f.operator]} {_box_value_as_full_iri(value)})"
-                    )
-        except NotImplementedError as nie:
-            raise ValueError(
-                f"Operator '{sparql_operator_map[f.operator]}' is not supported with the value '{f.value}': {str(nie)}")
-    return statements, filters
-
-
-def _box_value_as_full_iri(value):
-    return f"<{value}>" if is_valid_url(value) else value
-
-
 def _create_select_query(vars_, statements, distinct, search_in_graph):
     where_clauses = (
         f"{{ Graph ?g {{{statements}}}}}" if search_in_graph else f"{{{statements}}}"
