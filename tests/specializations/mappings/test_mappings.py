7d677cca9e8909e221fe65cdce5bc1e0fd3c86ca
mouffok <sarah.mouffok@epfl.ch>
2023-10-19 09:14:46 +0200

    added test cases for mapping type specific methods

--- tests/specializations/mappings/test_mappings.py
+++ tests/specializations/mappings/test_mappings.py
@@ -18,6 +18,8 @@
 import pytest
 from contextlib import nullcontext as does_not_raise
 
+from requests import RequestException
+
 from kgforge.core.archetypes.mapping import MappingType
 from kgforge.specializations.mappings import DictionaryMapping
 from utils import full_path_relative_to_root
@@ -28,6 +30,10 @@ mapping_url_valid = "https://raw.githubusercontent.com/BlueBrain/nexus-forge/mas
 mapping_path_valid = full_path_relative_to_root(
     "tests/data/nexus-store/file-to-resource-mapping.hjson"
 )
+
+mapping_text_valid = "{}"
+
+mapping_text_invalid = "i"
 
 
 @pytest.mark.parametrize(
@@ -47,9 +53,57 @@ def test_mapping_load_no_mapping_type(source, exception):
     [
         (mapping_path_valid, MappingType.FILE, does_not_raise()),
         (mapping_url_valid, MappingType.URL, does_not_raise()),
+        (mapping_path_valid, MappingType.URL, pytest.raises(RequestException)),
+        (mapping_url_valid, MappingType.FILE, pytest.raises(FileNotFoundError)),
+        (mapping_path_valid, MappingType.STR, pytest.raises(Exception)),
+        (mapping_url_valid, MappingType.STR, pytest.raises(Exception)),
         ("i", MappingType.URL, pytest.raises(Exception)),
     ],
 )
 def test_mapping_load_mapping_type(source, mapping_type, exception):
     with exception:
         mapping = DictionaryMapping.load(source, mapping_type)
+
+
+@pytest.mark.parametrize(
+    "source, exception",
+    [
+        (mapping_path_valid, does_not_raise()),
+        (mapping_url_valid, pytest.raises(FileNotFoundError)),
+        (mapping_text_invalid, pytest.raises(FileNotFoundError)),
+        (mapping_text_valid, pytest.raises(FileNotFoundError)),
+    ],
+)
+def test_mapping_load_file(source, exception):
+    with exception:
+        mapping = DictionaryMapping.load_file(source)
+
+
+@pytest.mark.parametrize(
+    "source, exception",
+    [
+        (mapping_url_valid, does_not_raise()),
+        (mapping_path_valid, pytest.raises(RequestException)),
+        (mapping_text_invalid, pytest.raises(RequestException)),
+        (mapping_text_valid, pytest.raises(RequestException)),
+
+    ],
+)
+def test_mapping_load_url(source, exception):
+    with exception:
+        mapping = DictionaryMapping.load_url(source)
+
+
+@pytest.mark.parametrize(
+    "source, exception",
+    [
+        (mapping_path_valid, pytest.raises(Exception)),
+        (mapping_url_valid, pytest.raises(Exception)),
+        (mapping_text_invalid, pytest.raises(Exception)),
+        (mapping_text_valid, does_not_raise()),
+
+    ],
+)
+def test_mapping_load_text(source, exception):
+    with exception:
+        mapping = DictionaryMapping.load_text(source)
