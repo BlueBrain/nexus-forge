a87072ed4cc7618fe8451f27269460745a8e79e1
mouffok <sarah.mouffok@epfl.ch>
2024-05-30 18:12:03 +0200

    split prepare download one

--- kgforge/specializations/stores/bluebrain_nexus.py
+++ kgforge/specializations/stores/bluebrain_nexus.py
@@ -554,21 +554,7 @@ class BlueBrainNexus(Store):
             for chunk in response.iter_content(chunk_size=4096):
                 f.write(chunk)
 
-    def _prepare_download_one(
-        self, url: str, store_metadata: Optional[DictWrapper], cross_bucket: bool
-    ) -> Tuple[str, str]:
-        if cross_bucket:
-            if store_metadata is not None:
-                project = store_metadata._project.split("/")[-1]
-                org = store_metadata._project.split("/")[-2]
-            else:
-                raise ValueError(
-                    f"Downloading non registered file is not allowed when cross_bucket is set to {cross_bucket}"
-                )
-        else:
-            org = self.service.organisation
-            project = self.service.project
-
+    def _prepare_download_one_with_org_project(self, url: str, org: str, project: str):
         file_id = url.split("/")[-1]
         file_id = unquote(file_id)
         if len(file_id) < 1:
@@ -589,6 +575,24 @@ class BlueBrainNexus(Store):
                 )
             )
         return url_base, f"{org}/{project}"
+
+    def _prepare_download_one(
+        self, url: str, store_metadata: Optional[DictWrapper], cross_bucket: bool
+    ) -> Tuple[str, str]:
+        if cross_bucket:
+            if store_metadata is not None:
+                org, project = store_metadata._project.split("/")
+            else:
+                raise ValueError(
+                    f"Downloading non registered file is not allowed when cross_bucket is set to True"
+                )
+        else:
+            org = self.service.organisation
+            project = self.service.project
+
+        return self._prepare_download_one_with_org_project(url, org, project)
+
+
 
     # CR[U]D.
 
